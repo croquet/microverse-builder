e6a7bcfd2c55069a7cf59c6fa7b20a9c8117d29b
Dennis Au <e46mpwr@users.noreply.github.com>
2022-04-29 14:26:21 -0700

    Create apiKey.js

---
+++ apiKey.js
@@ -0,0 +1,5 @@
+const apiKey = "1ejyvrJv62CBgzB9haPx3xoBMeKr5JvvFnxsKxgDM";
+const appId = "us.derp.metaverse";
+export default {apiKey, appId};
+
+// you may export other Croquet session parameters to override default values.
