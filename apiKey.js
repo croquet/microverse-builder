e0f1e447f886ce6557e62eeb6cae827323b563ac
Gälli Markus, B2B-BAN-PDC-SWE-3 <Markus.Gaelli@swisscom.com>
2022-05-26 23:37:29 +0200

    typo

---
+++ apiKey.js
@@ -0,0 +1,4 @@
+/* Copy this into a file named apiKey.js */
+const apiKey = '1eBC2qdrgpJKcz3z4L4nlcxJ0gvnblD2vGsNybbGs';
+const appId = 'com.gmail.markus.gaelli.microverse';
+export default {apiKey, appId};
