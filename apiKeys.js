96ec862d2042ab9394dfb292eadf2e49b222d7aa
Gälli Markus, B2B-BAN-PDC-SWE-3 <Markus.Gaelli@swisscom.com>
2022-05-26 23:25:32 +0200

    apiKeys

--- apiKeys.js
+++ apiKeys.js
@@ -1,5 +1,4 @@
-const apiKey = "1eBC2qdrgpJKcz3z4L4nlcxJ0gvnblD2vGsNybbGs";
-const appId = "com.swisscom.playground";
+/* Copy this into a file named apiKey.js */
+const apiKey = '1eBC2qdrgpJKcz3z4L4nlcxJ0gvnblD2vGsNybbGs';
+const appId = 'com.gmail.markus.gaelli.microverse';
 export default {apiKey, appId};
-
-// you may export other Croquet session parameters to override default values.
