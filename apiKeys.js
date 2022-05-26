fb13be34d5d694b0f912f9d1279cac8c07240418
Gälli Markus, B2B-BAN-PDC-SWE-3 <Markus.Gaelli@swisscom.com>
2022-05-26 23:12:57 +0200

    add apiKeys, remove package.json

---
+++ apiKeys.js
@@ -0,0 +1,5 @@
+const apiKey = "1eBC2qdrgpJKcz3z4L4nlcxJ0gvnblD2vGsNybbGs";
+const appId = "com.swisscom.playground";
+export default {apiKey, appId};
+
+// you may export other Croquet session parameters to override default values.
