3c87f8306b08859901e7891bf9e9a8edba3d3941
TrevorDohm <78062983+TrevorDohm@users.noreply.github.com>
2022-05-31 16:12:11 -0700

    New Branch W Changes

---
+++ behaviors/race/car.js
@@ -0,0 +1,126 @@
+// Car (Driver)
+// Copyright 2022 Croquet Corporation
+// Croquet Microverse
+
+class DriveActor {
+
+    setup() {
+        if (this.speed === undefined) this.speed = 0.0;
+        if (this.accel === undefined) this.accel = 0.0;
+        if (this.angle === undefined) this.angle = 0.0;
+        if (this.running === undefined) {
+            this.running = true;
+            this.run(); }
+        this.addEventListener("keyDown", "control");
+        this.addEventListener("keyUp", "endControl");
+        this.addEventListener("pointerDown", "ride");
+        this.subscribe(this._cardData.myScope, "newAngle", "newAngle");
+        this.subscribe(this._cardData.myScope, "newSpeed", "newSpeed");
+    }
+
+    run() {
+        if (!this.running) { return; }
+        this.future(20).run();
+        this.speed = this.speed + this.accel; // Add current acceleration to speed
+        if (this.speed > 2.0) { // Speed limits
+            this.speed = 1.0; }
+        if (this.speed < -0.2) {
+            this.speed = -0.2; }
+        // if (this.speed != 0.0) { // Speed slowdown
+        //     this.speed /= 1.010; }
+        // if (this.angle != 0.0) { // Angle slowdown
+        //     this.angle /= 1.010; }
+        this.rotateBy([0, -this.angle, 0]);
+        this.forwardBy(-this.speed);
+        if (this.avatar) {
+            let t = this._translation;
+            this.avatar._translation = [t[0], t[1] + 1.5, t[2] + 0.3];
+            this.avatar._rotation = this._rotation;
+            this.avatar.say("forceOnPosition");
+        }
+    }
+
+    ride() {
+        let actors = this.queryCards();
+        let avatar = actors.find(o => o.layers.includes("avatar"));
+        this.avatar = avatar;
+        this.riding = true;
+    }
+
+    newAngle(angle) {
+        angle = angle / 20;
+        this.angle = angle;
+    }
+
+    newSpeed(speed) {
+        speed = speed / 5;
+        this.speed = speed;
+    }
+
+    rotateBy(angles) {
+        let q = Worldcore.q_euler(...angles);
+        q = Worldcore.q_multiply(this.rotation, q);
+        this.rotateTo(q);
+    }
+
+    forwardBy(dist) {
+        let v = Worldcore.v3_rotate([0, 0, dist], this.rotation);
+        this.translateTo([
+            this.translation[0] + v[0],
+            this.translation[1] + v[1],
+            this.translation[2] + v[2]]);
+    }
+
+    control(key) {
+        if (key.key === "ArrowRight") { // Right/Left arrow keys spin the car while held
+            this.angle = 0.02;
+        } else if (key.key === "ArrowLeft") {
+            this.angle = -0.02;
+        } else if (key.key === "ArrowUp") { // Up/Down arrow keys accelerate the car while held
+            this.accel = 0.01;
+        } else if (key.key === "ArrowDown") {
+            this.accel = -0.02;
+        } else if (key.key === "Shift") { // Shift/Escape arrow keys handle exiting and resetting
+            this.avatar = undefined;
+            this.riding = false;
+        } else if (key.key === "Escape") {
+            this.translateTo(this._cardData.translation);
+            this.rotateTo(this._cardData.dataRotation);
+            this.speed = 0.0;
+            this.accel = 0.0;
+            this.angle = 0.0;
+            this.avatar = undefined;
+            this.riding = false;
+        }
+    }
+
+    endControl(key){
+        if (key.key === "ArrowRight") { // Stopping spin on release
+            this.angle = 0.0;
+        } else if (key.key === "ArrowLeft") {
+            this.angle = 0.0;
+        } else if (key.key === "ArrowUp") { // Stopping acceleration on release
+            this.accel = 0.0;
+        } else if (key.key === "ArrowDown") {
+            this.accel = 0.0;
+        } 
+    }
+
+    destroy() {
+        this.removeEventListener("pointerDown", "toggle");
+        this.removeEventListener("keyDown", "turn", "keyUp");
+        this.running = false;
+    }
+    
+}
+
+export default {
+    modules: [
+        {
+            name: "Drive",
+            actorBehaviors: [DriveActor]
+        }
+    ]
+}
+
+/* globals Worldcore */
---
+++ worlds/race.js
@@ -0,0 +1,157 @@
+// Copyright 2022 by Croquet Corporation, Inc. All Rights Reserved.
+// https://croquet.io
+// info@croquet.io
+
+export function init(Constants) {
+    Constants.AvatarNames = [
+        "newwhite", "madhatter", "marchhare", "queenofhearts", "cheshirecat", "alice"
+    ];
+
+    Constants.SystemBehaviorDirectory = "behaviors/croquet";
+    Constants.SystemBehaviorModules = [
+        "menu.js", "elected.js", "propertySheet.js", "stickyNote.js", "rapier.js", "avatar.js"
+    ];
+
+    Constants.UserBehaviorDirectory = "behaviors/race";
+    Constants.UserBehaviorModules = [
+        "car.js", "lights.js", "controller.js", "cascade.js"
+    ];
+
+    Constants.UseRapier = true;
+
+    Constants.DefaultCards = [
+        {
+            card: {
+                name:"world model",
+                dataScale:[1, 1, 1],
+                translation:[0, -1.8, 0],
+                layers: ["walk"],
+                type: "3d",
+                dataLocation: "./assets/3D/tsukuba2racetrack.glb.zip",
+                singleSided: true,
+                shadow: true,
+                placeholder: true,
+                placeholderSize: [100, 0.01, 100],
+                placeholderColor: 0xcccccc,
+                placeholderOffset: [0, 0, 0],
+
+            }
+        },
+        {
+            card: {
+                name: "light",
+                layers: ["light"],
+                type: "lighting",
+                behaviorModules: ["Light"],
+                dataLocation: "./assets/sky/night_sky_4k.jpg",
+                dataType: "jpg",
+            }
+        },
+        {
+            card: {
+                name:"racecarone",
+                dataRotation: [0, -Math.PI / 2, 0],
+                translation: [0, -1.672, -10],
+                layers: ["pointer"],
+                type: "3d",
+                multiuser: true,
+                dataLocation: "./assets/3D/porscheBlue.glb.zip",
+                behaviorModules: ["Rapier", "Drive"], 
+                myScope: "A",
+                color: 0x0000ff
+            }
+        },
+        {
+            card: {
+                name:"racecartwo",
+                dataRotation: [0, -Math.PI / 2, 0],
+                translation: [5, -1.672, -10],
+                layers: ["pointer"],
+                type: "3d",
+                multiuser: true,
+                dataLocation: "./assets/3D/porscheRed.glb.zip",
+                behaviorModules: ["Rapier", "Drive"],
+                myScope: "B",
+                color: 0xff0000
+            }
+        },
+        {
+            card: {
+                name:"controllerone",
+                type: "3d",
+                translation: [-5, 0.4, -10],
+                behaviorModules: ["Controller"],
+                layers: ["pointer"],
+                shadow: true,
+                myScope: "A",
+                multiuser: false,
+                color: 0x0000ff
+            }
+        },
+        {
+            card: {
+                name:"controllertwo",
+                type: "3d",
+                translation: [10, 0.4, -10],
+                behaviorModules: ["Controller"],
+                layers: ["pointer"],
+                shadow: true,
+                myScope: "B",
+                multiuser: false,
+                color: 0xff0000
+            }
+        },
+        // {
+        //     card: {
+        //         name:"block1",
+        //         type: "3d",
+        //         translation: [10, 10, -15],
+        //         behaviorModules: ["Rapier", "Cascade"],
+        //         layers: ["walk"],
+        //         rapierSize: [2, 2, 2],
+        //         rapierShape: "cuboid",
+        //         rapierType: "dynamic",
+        //         color: 0x000077,
+        //     }
+        // },
+        // {
+        //     card: {
+        //         name:"block2",
+        //         type: "3d",
+        //         translation: [10, -0.7, -15],
+        //         behaviorModules: ["Rapier", "Cascade"],
+        //         layers: ["walk"],
+        //         rapierSize: [2, 2, 2],
+        //         rapierShape: "cuboid",
+        //         rapierType: "static",
+        //         color: 0x007700,
+        //     }
+        // },
+        // {
+        //     card: {
+        //         name:"block3",
+        //         type: "3d",
+        //         translation: [5, 0, -15],
+        //         behaviorModules: ["Rapier", "Cascade", "Drive"],
+        //         layers: ["walk", "pointer"],
+        //         rapierSize: [2, 1, 2],
+        //         rapierShape: "cuboid",
+        //         rapierType: "dynamic",
+        //         color: 0x770000,
+        //     }
+        // },
+        // {
+        //     card: {
+        //         name:"block4",
+        //         type: "3d",
+        //         translation: [5, 2, -20],
+        //         behaviorModules: ["Rapier", "Cascade"],
+        //         layers: ["walk"],
+        //         rapierSize: [2, 2, 2],
+        //         rapierShape: "cuboid",
+        //         rapierType: "dynamic",
+        //         color: 0x000077,
+        //     }
+        // },
+    ];
+}
