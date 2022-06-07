1e8ce885235c48cfa4a917e7e4faf1358a561818
TrevorDohm <78062983+TrevorDohm@users.noreply.github.com>
2022-06-06 18:15:25 -0700

    Adding race track and world changes

--- worlds/race.js
+++ worlds/race.js
@@ -23,13 +23,14 @@ export function init(Constants) {
         {
             card: {
                 name:"world model",
-                dataScale:[0.6, 0.6, 0.6],
-                translation:[0, -1.8, 0],
+                dataScale:[18, 18, 18], // dataScale:[0.6, 0.6, 0.6]
+                translation:[-12.2, -22.8, -14.2], // translation:[0, -1.8, 0],
                 layers: ["walk"],
                 type: "3d",
-                dataLocation: "./assets/3D/tsukuba2racetrack.glb.zip",
+                dataLocation: "./assets/3D/tsukuba2racetrackwide.glb.zip",
                 singleSided: true,
                 shadow: true,
+                fullBright: true,
                 placeholder: true,
                 placeholderSize: [100, 0.01, 100],
                 placeholderColor: 0xcccccc,
@@ -43,7 +44,7 @@ export function init(Constants) {
                 layers: ["light"],
                 type: "lighting",
                 behaviorModules: ["Light"],
-                dataLocation: "./assets/sky/abandoned_parking_4k.jpg",
+                dataLocation: "./assets/sky/shanghai_riverside_2k.jpg",
                 dataType: "jpg",
             }
         },
@@ -51,8 +52,8 @@ export function init(Constants) {
             card: {
                 name:"racecarone",
                 dataRotation: [0, -Math.PI / 2, 0],
-                translation: [0, -1.672, -10],
-                startPoint: [0, -1.672, -10],
+                translation: [-58.5, -1.672, 24],
+                startPoint: [-58.5, -1.672, 24],
                 layers: ["pointer"],
                 type: "3d",
                 multiuser: true,
@@ -66,8 +67,8 @@ export function init(Constants) {
             card: {
                 name:"racecartwo",
                 dataRotation: [0, -Math.PI / 2, 0],
-                translation: [5, -1.672, -10],
-                startPoint: [5, -1.672, -10],
+                translation: [-55, -1.672, 30],
+                startPoint: [-55, -1.672, 30],
                 layers: ["pointer"],
                 type: "3d",
                 multiuser: true,
@@ -77,11 +78,26 @@ export function init(Constants) {
                 color: 0xff0000
             }
         },
+        // {
+        //     card: {
+        //         name:"racecarthree",
+        //         dataRotation: [0, -Math.PI / 2, 0],
+        //         translation: [-58.5, -1.672, 30],
+        //         startPoint: [5, -1.672, -10],
+        //         layers: ["pointer"],
+        //         type: "3d",
+        //         multiuser: true,
+        //         dataLocation: "./assets/3D/porscheRed.glb.zip",
+        //         behaviorModules: ["Rapier", "Drive"],
+        //         myScope: "C",
+        //         color: 0xff0000
+        //     }
+        // },
         {
             card: {
                 name:"controllerone",
                 type: "3d",
-                translation: [10.2, 195, -12],
+                translation: [0, 115, -6.9], // translation: [10.2, 195, -12],
                 behaviorModules: ["Controller"],
                 layers: ["pointer"],
                 shadow: true,
@@ -94,7 +110,7 @@ export function init(Constants) {
             card: {
                 name:"controllertwo",
                 type: "3d",
-                translation: [10.2, 195, 7.9],
+                translation: [0, 115, 2.8], // translation: [10.2, 195, 7.9],
                 behaviorModules: ["Controller"],
                 layers: ["pointer"],
                 shadow: true,
@@ -108,7 +124,7 @@ export function init(Constants) {
                 name:"teleporterone",
                 type: "3d",
                 translation: [-5, 0.4, -10],
-                teleportLocation: [10, 200, -10],
+                teleportLocation: [0, 120, -5],
                 teleportPitch: -(Math.PI / 2),
                 teleportYaw: -(Math.PI / 2),
                 behaviorModules: ["Teleporter"],
@@ -124,7 +140,7 @@ export function init(Constants) {
                 name:"teleportertwo",
                 type: "3d",
                 translation: [10, 0.4, -10],
-                teleportLocation: [10, 200, 10],
+                teleportLocation: [0, 120, 5],
                 teleportPitch: -(Math.PI / 2),
                 teleportYaw: -(Math.PI / 2),
                 behaviorModules: ["Teleporter"],
@@ -139,7 +155,7 @@ export function init(Constants) {
             card: {
                 name:"teleporteroneback",
                 type: "3d",
-                translation: [11.8, 195, -7.2],
+                translation: [1.8, 115, -2.2], // translation: [11.8, 115, -7.2],
                 teleportLocation: [0, 0, 0],
                 teleportPitch: 0,
                 teleportYaw: -(Math.PI / 2),
@@ -155,7 +171,7 @@ export function init(Constants) {
             card: {
                 name:"teleportertwoback",
                 type: "3d",
-                translation: [11.8, 195, 12.6],
+                translation: [1.8, 115, 7.4], // translation: [11.8, 195, 12.6],
                 teleportLocation: [0, 0, 0],
                 teleportPitch: 0,
                 teleportYaw: -(Math.PI / 2),
