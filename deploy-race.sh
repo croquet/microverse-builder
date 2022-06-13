7db4fd64b0bf13082e6512fcf5fc781dd587084d
Yoshiki Ohshima <yoshiki.ohshima@acm.org>
2022-06-13 15:48:23 -0700

    revise deploy script

--- deploy-race.sh
+++ deploy-race.sh
@@ -8,8 +8,18 @@ then
     exit 1;
 fi
 
-rsync -r lib index.html assets $DIST
+rsync -r lib index.html $DIST
 rsync -r behaviors/race behaviors/croquet ${DIST}/behaviors
+
+mkdir -p ${DIST}/assets/3D
+
+rsync -r assets/3D/{porscheBlue.glb.zip,porscheGreen.glb.zip,porscheRed.glb.zip,porscheYellow.glb.zip,tsukuba2racetrack.glb.zip,tsukuba2racetrackbase.glb.zip} ${DIST}/assets/3D/
+rsync -r assets/{avatars,css,fonts} ${DIST}/assets/
+rsync -r assets/sky/shanghai_riverside_2k.jpg ${DIST}/assets/sky/
+rsync -r assets/images/grid.png ${DIST}/assets/images/
+
+mkdir -p ${DIST}/meta
+cp meta/version.txt ${DIST}/meta
 
 mkdir -p ${DIST}/worlds
 cp worlds/race.js ${DIST}/worlds/default.js
